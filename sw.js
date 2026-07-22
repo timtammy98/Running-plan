3b345fdd13630a66c2d8cba2cfc01b77871ad64b
timtammy98 <tmnguyen98@icloud.com>
2026-07-22 17:23:04 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='runplan-v2';
+const CACHE='runplan-v3';
 const FILES=['./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
